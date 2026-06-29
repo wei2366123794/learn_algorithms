7929787beab987e8608d5b0543e8d18cb31546b4
wlcsss <2366123794@qq.com>
2026-06-29 23:25:36 +0800

    init

--- 01.cpp
+++ 01.cpp
@@ -3,22 +3,33 @@
 // 但是海盗船的容量c是有限的，每件宝物的容量为w[i], 怎么样能够装最多的宝物
 
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+int getMax(int c, vector<int> bw);
+
 int main() {
 
     // cout << "hello";
+    vector<int> bw = { 5, 3, 7, 9, 1 };
 
-
+    getMax(20, bw);
 
 
 
 }
 
 
-int getMax(int c, int bw[]) {
+int getMax(int c, vector<int> bw) {
     int len = bw.size();
 
+    sort(bw.begin(), bw.end());
+
+    for (int i = 0; i < bw.size(); i++) {
+        cout << bw[i] << " ";
+    }
+
+    return 0;
 }
