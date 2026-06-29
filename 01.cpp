dfb350507360cf0ecbb190af766468aa88b22b91
wlcsss <2366123794@qq.com>
2026-06-29 23:14:12 +0800

    init

---
+++ 01.cpp
@@ -0,0 +1,24 @@
+// 最优装载问题
+// 海盗截获了一艘货船，里面有很多价值不菲的宝物，海盗想尽可能的装更多的宝物
+// 但是海盗船的容量c是有限的，每件宝物的容量为w[i], 怎么样能够装最多的宝物
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    // cout << "hello";
+
+
+
+
+
+}
+
+
+int getMax(int c, int bw[]) {
+    int len = bw.size();
+
+}
